9ca9b9df85de14b4e5bb47295e38c4499553474e
Bisrat Gizaw <bisrojc60@gmail.com>
2025-06-01 19:00:28 +0300

    remove

--- alx-project-0x00/src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function HomePage() {
-  return (
-    <main className="flex min-h-screen items-center justify-center bg-white text-black">
-      <h1 className="text-4xl font-bold">Welcome to ALX Project 0x00</h1>
-    </main>
-  );
-}
